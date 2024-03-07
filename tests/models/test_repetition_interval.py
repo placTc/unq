7d9bdce38dca1972a56886a52eeb129f13192d5f
Egor Matuk <gregory.matuk2004@gmail.com>
2024-03-07 21:16:31 +0200

    ✅ added repetition interval tests

---
+++ tests/models/test_repetition_interval.py
@@ -0,0 +1,29 @@
+from dataclasses import FrozenInstanceError
+from unq import RepetitionInterval
+import pytest
+
+@pytest.fixture
+def default_ri():
+    # Arrange
+    timeframe = "second"
+    every = 6
+    times = 6
+    return RepetitionInterval(timeframe=timeframe, every=every, times=times)
+
+
+def test_constructor_properly_creates_object(default_ri):
+    # Arrange
+    timeframe = "second"
+    every = 6
+    times = 6
+    
+    # Assert
+    assert default_ri.timeframe == timeframe, "Timeframe did not match"
+    assert default_ri.every == every, "Every did not match"
+    assert default_ri.times == times, "Times did not match"
+    
+    
+def test_object_is_immutable_dataclass(default_ri):
+    # Act, Assert
+    with pytest.raises(FrozenInstanceError):
+        default_ri.timeframe = "hour"
