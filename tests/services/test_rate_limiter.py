5398f0f2a56cd6dd88a4d0aafac18b560c956529
Egor Matuk <gregory.matuk2004@gmail.com>
2024-03-09 18:26:02 +0200

    ✅ Added function submit tests

--- tests/services/test_rate_limiter.py
+++ tests/services/test_rate_limiter.py
@@ -1,9 +1,20 @@
+from asyncio import wait_for, wait
+import time
+from unittest.mock import Mock
 import pytest
 from unq import RateLimiter
 from unq.models.repetition_interval import RepetitionInterval
 
 @pytest.fixture(autouse=True)
 def default_rate_limiter():
+    rate_limiter = RateLimiter(repetition_interval=0.5)
+    yield rate_limiter
+    
+    rate_limiter.stop()
+    
+# This fixture is needed in order to have a local event loop defined when running async tests
+@pytest.fixture(autouse=True)
+async def default_async_rate_limiter():
     rate_limiter = RateLimiter(repetition_interval=0.5)
     yield rate_limiter
     
@@ -52,24 +63,55 @@ def test_constructor_raises_when_provided_unparsable_type():
     with pytest.raises(TypeError):
         RateLimiter(repetition_interval=rep_interval)
         
-def test_marked_stopped_when_constructed(default_rate_limiter):
+        
+def test_marked_stopped_when_constructed(default_rate_limiter: RateLimiter):
     # Assert
     assert default_rate_limiter.stopped == True, "Somehow, it was started"
     
     
-def test_marked_not_stopped_when_started(default_rate_limiter):
+def test_marked_stopped_when_stopped_without_being_started(default_rate_limiter: RateLimiter):
+    # Act
+    default_rate_limiter.stop()
+    
+    # Assert
+    assert default_rate_limiter.stopped == True, "Somehow, it was started"
+
+    
+def test_marked_not_stopped_when_started(default_rate_limiter: RateLimiter):
     # Act
     default_rate_limiter.start()
     
     # Assert
-    assert default_rate_limiter.stopped == False, "Somehow, the class is stopped"
+    assert default_rate_limiter.stopped == False, "Somehow, it was stopped"
     
     
-def test_marked_stopped_when_stopped_after_started(default_rate_limiter):
+def test_marked_stopped_when_stopped_after_started(default_rate_limiter: RateLimiter):
     # Act
     default_rate_limiter.start()
     default_rate_limiter.stop()
     
     # Assert
-    assert default_rate_limiter._internal_runner_thread.is_alive() == False
     assert default_rate_limiter.stopped == True, "Somehow, it was not stopped"
+  
+async def test_function_executed_after_submit_in_async_context(default_async_rate_limiter: RateLimiter):
+    # Arrange
+    default_async_rate_limiter.start()
+    mock_function = Mock(return_value="Test Test")
+    
+    # Act
+    await default_async_rate_limiter.submit(mock_function)
+    
+    # Assert
+    assert mock_function.called
+    
+def test_function_executed_after_submit(default_rate_limiter: RateLimiter):
+    # Arrange
+    default_rate_limiter.start()
+    mock_function = Mock(return_value="Test Test")
+    
+    # Act
+    default_rate_limiter.submit(mock_function)
+    time.sleep(2) # We don't want to undershoot
+    
+    # Assert
+    assert mock_function.called
